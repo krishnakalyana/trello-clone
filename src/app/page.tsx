090e519e066267fd5946876142c6c0d17f53f8ea
krishnakalyana <krishna@sflhub.com>
2024-01-29 13:32:50 +0530

    mongo started

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
 import Board from "@/components/customComponents/Board";
+import { authOptions } from "@/lib/authOptions";
+import { getServerSession } from "next-auth";
 
-export default function Home() {
+export default async function Home() {
+  const session = await getServerSession(authOptions)
   return (
    <div>
     <Board/>
